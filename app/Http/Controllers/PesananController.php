9101ca947945ace5e4dd1d1fbd29abd1665570f7
PiperBelly08 <hawarimunte@gmail.com>
2025-08-28 07:37:03 +0700

    feat: enhance PesananController update method with file upload and status validation

--- app/Http/Controllers/PesananController.php
+++ app/Http/Controllers/PesananController.php
@@ -70,28 +70,48 @@ class PesananController extends Controller
      */
     public function update(Request $request, Pesanan $pesanan)
     {
+        if (!($pesanan->status == 'Pending') || !($pesanan->status == 'Diproses')) {
+            if ($request->hasFile('file')) {
+                $file = $request->file('file');
+                $filename = sprintf('SIPEMPOL_%s.%s', time(), $file->getClientOriginalExtension());
+                $file->move(public_path('images'), $filename);
+                $pesanan->update(['file_desain' => $filename]);
+            }
+
+            $validated = $request->validate([
+                'status' => 'required|in:Pending,Diproses,Selesai,Dibatalkan',
+                'tanggal_pesan' => 'required|date',
+                'tanggal_selesai' => 'nullable|date|after_or_equal:tanggal_pesan',
+                'total_harga' => 'required|numeric|min:5000',
+            ]);
+
+            $pesanan->update($validated);
+            return redirect()->route('pesanan.index')->with('success', 'Pesanan berhasil diperbarui.');
+        }
+
         if ($pesanan->status == 'Pending') {
             $validated = $request->validate([
-                'status' => 'required|in:Diproses,Ditolak',
+                'status' => 'required|in:Diproses,Dibatalkan',
+                'tanggal_pesan' => 'required|date',
+                'tanggal_selesai' => 'nullable|date|after_or_equal:tanggal_pesan',
+                'total_harga' => 'required|numeric|min:5000',
             ]);
 
             $pesanan->update(['status' => $validated['status']]);
-
             return redirect()->route('pesanan.index')->with('success', 'Pesanan berhasil Diperbarui.');
         }
 
-        $validated = $request->validate([
-            'layanan_id' => 'required|exists:App\Models\Layanan,id',
-            'pelanggan_id' => 'required|exists:App\Models\User,id',
-            'jumlah_pemesanan' => 'required|numeric|min:1',
-            'deskripsi_pesan' => 'nullable|string',
-            'tanggal_pesan' => 'required|date',
-            'tanggal_selesai' => 'nullable|date|after_or_equal:tanggal_pesan',
-            'total_harga' => 'required|numeric|min:1',
-        ]);
+        if ($pesanan->status == 'Diproses') {
+            $validated = $request->validate([
+                'status' => 'required|in:Selesai,Dibatalkan',
+                'tanggal_pesan' => 'required|date',
+                'tanggal_selesai' => 'nullable|date|after_or_equal:tanggal_pesan',
+                'total_harga' => 'required|numeric|min:5000',
+            ]);
 
-        $pesanan->update($validated);
-        return redirect()->route('pesanan.index')->with('success', 'Pesanan berhasil diperbarui.');
+            $pesanan->update(['status' => $validated['status']]);
+            return redirect()->route('pesanan.index')->with('success', 'Pesanan berhasil Diperbarui.');
+        }
     }
 
     /**
--- resources/views/pages/pesanans/index.blade.php
+++ resources/views/pages/pesanans/index.blade.php
@@ -32,34 +32,78 @@
                 <td>{{ $pesanan->pelanggan->nama }}</td>
                 <td>{{ $pesanan->layanan->nama }}</td>
                 <td>
-                    <div class="d-flex gap-1">
-                        @switch($pesanan->status)
-                            @case('Pending')
-                                <span class="badge rounded-pill bg-warning text-dark">{{ $pesanan->status }}</span>
-                            @break
-
-                            @case('Diproses')
-                                <span class="badge rounded-pill bg-primary">{{ $pesanan->status }}</span>
-                            @break
-
-                            @case('Selesai')
-                                <span class="badge rounded-pill bg-success">{{ $pesanan->status }}</span>
-                            @break
-
-                            @case('Dibatalkan')
-                                <span class="badge rounded-pill bg-danger">{{ $pesanan->status }}</span>
-                            @break
-                        @endswitch
-                    </div>
+                    @switch($pesanan->status)
+                        @case('Pending')
+                            <span class="badge rounded-pill bg-warning text-dark">{{ $pesanan->status }}</span>
+                        @break
+                        @case('Diproses')
+                            <span class="badge rounded-pill bg-primary">{{ $pesanan->status }}</span>
+                        @break
+                        @case('Selesai')
+                            <span class="badge rounded-pill bg-success">{{ $pesanan->status }}</span>
+                        @break
+                        @case('Dibatalkan')
+                            <span class="badge rounded-pill bg-danger">{{ $pesanan->status }}</span>
+                        @break
+                    @endswitch
                 </td>
                 <td>{{ $pesanan->deskripsi_pesan }}</td>
-                <td>{{ $pesanan->file_desain }}</td>
+                <td>
+                    @if(!isset($pesanan->file_desain))
+                        <span class="badge rounded-pill bg-danger">Belum Upload</span>
+                    @else
+                        <button type="button" class="btn btn-sm btn-primary" data-bs-toggle="modal" data-bs-target="#showFile{{ $pesanan->id }}">
+                            <i class="bi bi-eye"></i>
+                        </button>
+                        <div class="modal fade" id="showFile{{ $pesanan->id }}" tabindex="-1" aria-labelledby="showFileLabel" aria-hidden="true">
+                            <div class="modal-dialog">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h5 class="modal-title" id="showFileLabel">File Desain</h5>
+                                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                    </div>
+                                    <div class="modal-body">
+                                        <img src="{{ 'images/' . $pesanan->file_desain }}" class="img-fluid" alt="File Desain" loading="lazy">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    @endif
+                </td>
                 <td>{{ $pesanan->tanggal_pesan }}</td>
                 <td>{{ $pesanan->tanggal_selesai }}</td>
                 <td>{{ $pesanan->total_harga }}</td>
                 <td>
                     <div class="d-flex gap-2">
                         @role('customer')
+                        <button type="button" class="btn btn-sm btn-info" data-bs-toggle="modal" data-bs-target="#uploadDesain{{ $pesanan->id }}">
+                            <i class="bi bi-upload"></i>
+                        </button>
+                        <div class="modal fade" id="uploadDesain{{ $pesanan->id }}" tabindex="-1" aria-labelledby="uploadDesainLabel" aria-hidden="true">
+                            <div class="modal-dialog">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h5 class="modal-title" id="uploadDesainLabel">Upload Desain</h5>
+                                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                    </div>
+                                    <div class="modal-body">
+                                        <form action="{{ route('pesanan.update', $pesanan->id) }}" method="post" enctype="multipart/form-data">
+                                            @csrf
+                                            @method('put')
+                                            <input type="hidden" name="status" value="{{ $pesanan->status }}">
+                                            <input type="hidden" name="tanggal_pesan" value="{{ $pesanan->tanggal_pesan }}">
+                                            <input type="hidden" name="tanggal_selesai" value="{{ $pesanan->tanggal_selesai }}">
+                                            <input type="hidden" name="total_harga" value="{{ $pesanan->total_harga }}">
+                                            <div class="mb-3">
+                                                <label for="file" class="form-label">Desain</label>
+                                                <input class="form-control" type="file" id="file" name="file">
+                                            </div>
+                                            <button type="submit" class="btn btn-primary">Upload</button>
+                                        </form>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                         <button type="button" class="btn btn-sm btn-danger" data-bs-toggle="modal" data-bs-target="#deleteUser{{ $pesanan->id }}">
                             <i class="bi bi-trash"></i>
                         </button>
@@ -87,44 +131,129 @@
                         @endrole
 
                         @role('admin')
-
                         @if ($pesanan->status == 'Pending')
-                        <form action="{{ route('pesanan.update', $pesanan->id) }}" method="post" class="d-flex gap-2">
+                        <form action="{{ route('pesanan.update', $pesanan->id) }}" method="post" class="d-flex gap-2" onsubmit="return confirm('Yakin dengan pilihanmu?')">
                             @csrf
                             @method('put')
-                            <button type="submit" class="btn btn-sm btn-success" name="status" value="Diproses" title="terima?"><i class="bi bi-check-circle-fill"></i></button>
-                            <button type="submit" class="btn btn-sm btn-danger" name="status" value="Dibatalkan" title="tolak?"><i class="bi bi-x-circle-fill"></i></button>
+                            <input type="hidden" name="tanggal_pesan" value="{{ $pesanan->tanggal_pesan }}">
+                            <input type="hidden" name="tanggal_selesai" value="{{ $pesanan->tanggal_selesai }}">
+                            <input type="hidden" name="total_harga" value="5000">
+                            <button type="submit" class="btn btn-sm btn-success" name="status" value="Diproses" title="Terima pesanan">
+                                <i class="bi bi-check-circle-fill"></i>
+                            </button>
+                            <button type="submit" class="btn btn-sm btn-danger" name="status" value="Dibatalkan" title="Tolak Pesanan">
+                                <i class="bi bi-x-circle-fill"></i>
+                            </button>
                         </form>
                         @endif
 
-                        @if ($pesanan->status != 'Pending')
-                        <a href="{{ route('pesanan.show', $pesanan->id) }}" class="btn btn-sm btn-primary"><i class="bi bi-eye"></i></a>
-                        <a href="{{ route('pesanan.edit', $pesanan->id) }}" class="btn btn-sm btn-warning"><i class="bi bi-pencil"></i></a>
+                        @if ($pesanan->status == 'Diproses')
+                        <form action="{{ route('pesanan.update', $pesanan->id) }}" method="post" class="d-flex gap-2" onsubmit="return confirm('Yakin dengan pilihanmu?')">
+                            @csrf
+                            @method('put')
+                            <input type="hidden" name="status" value="{{ $pesanan->status }}">
+                            <input type="hidden" name="tanggal_pesan" value="{{ $pesanan->tanggal_pesan }}">
+                            <input type="hidden" name="tanggal_selesai" value="{{ $pesanan->tanggal_selesai }}">
+                            <input type="hidden" name="total_harga" value="{{ $pesanan->total_harga }}">
+                            <button type="submit" class="btn btn-sm btn-success" name="status" value="Selesai" title="Selesaikan pesanan">
+                                <i class="bi bi-check-circle-fill"></i>
+                            </button>
+                            <button type="submit" class="btn btn-sm btn-danger" name="status" value="Dibatalkan" title="Batalkan pesanan">
+                                <i class="bi bi-x-circle-fill"></i>
+                            </button>
+                        </form>
+                        @endif
 
-                        <button type="button" class="btn btn-sm btn-danger" data-bs-toggle="modal" data-bs-target="#deleteUser{{ $pesanan->id }}">
-                            <i class="bi bi-trash"></i>
-                        </button>
-                        <div class="modal fade" id="deleteUser{{ $pesanan->id }}" tabindex="-1" aria-labelledby="deleteUserLabel" aria-hidden="true">
-                            <div class="modal-dialog">
-                                <div class="modal-content">
-                                    <div class="modal-header">
-                                        <h5 class="modal-title" id="deleteUserLabel">Hapus Pengguna</h5>
-                                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                                    </div>
-                                    <div class="modal-body">
-                                        Apakah Anda yakin ingin menghapus pesanan ini?
-                                    </div>
-                                    <div class="modal-footer">
-                                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Batal</button>
-                                        <form action="{{ route('pesanan.destroy', $pesanan->id) }}" method="post">
-                                            @csrf
-                                            @method('delete')
-                                            <button type="submit" class="btn btn-danger">Hapus</button>
-                                        </form>
+                        @if ($pesanan->status != 'Pending' && $pesanan->status != 'Selesai' && $pesanan->status != 'Dibatalkan')
+                            <button type="button" class="btn btn-sm btn-info" data-bs-toggle="modal" data-bs-target="#uploadDesain{{ $pesanan->id }}">
+                                <i class="bi bi-upload"></i>
+                            </button>
+                            <div class="modal fade" id="uploadDesain{{ $pesanan->id }}" tabindex="-1" aria-labelledby="uploadDesainLabel" aria-hidden="true">
+                                <div class="modal-dialog">
+                                    <div class="modal-content">
+                                        <div class="modal-header">
+                                            <h5 class="modal-title" id="uploadDesainLabel">Upload Desain</h5>
+                                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                        </div>
+                                        <div class="modal-body">
+                                            <form action="{{ route('pesanan.update', $pesanan->id) }}" method="post" enctype="multipart/form-data">
+                                                @csrf
+                                                @method('put')
+                                                <input type="hidden" name="status" value="{{ $pesanan->status }}">
+                                                <input type="hidden" name="tanggal_pesan" value="{{ $pesanan->tanggal_pesan }}">
+                                                <input type="hidden" name="tanggal_selesai" value="{{ $pesanan->tanggal_selesai }}">
+                                                <input type="hidden" name="total_harga" value="{{ $pesanan->total_harga }}">
+                                                <div class="mb-3">
+                                                    <label for="file" class="form-label">Desain</label>
+                                                    <input class="form-control" type="file" id="file" name="file">
+                                                </div>
+                                                <button type="submit" class="btn btn-primary">Upload</button>
+                                            </form>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
-                        </div>
+
+                            <button type="button" class="btn btn-sm btn-warning" data-bs-toggle="modal" data-bs-target="#editPesanan{{ $pesanan->id }}">
+                                <i class="bi bi-pencil"></i>
+                            </button>
+                            <div class="modal fade" id="editPesanan{{ $pesanan->id }}" tabindex="-1" aria-labelledby="editPesananLabel" aria-hidden="true">
+                                <div class="modal-dialog">
+                                    <div class="modal-content">
+                                        <div class="modal-header">
+                                            <h5 class="modal-title" id="editPesananLabel">Perbarui Data</h5>
+                                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                        </div>
+                                        <div class="modal-body">
+                                            <form action="{{ route('pesanan.update', $pesanan->id) }}" method="post">
+                                                @csrf
+                                                @method('put')
+
+                                                <input type="hidden" name="status" value="{{ $pesanan->status }}">
+                                                <div class="mb-3">
+                                                    <label for="tanggal_pesan" class="form-label">Tanggal Pesan</label>
+                                                    <input class="form-control" type="date" id="tanggal_pesan" name="tanggal_pesan" value="{{ $pesanan->tanggal_pesan }}">
+                                                </div>
+                                                <div class="mb-3">
+                                                    <label for="tanggal_selesai" class="form-label">Tanggal Selesai</label>
+                                                    <input class="form-control" type="date" id="tanggal_selesai" name="tanggal_selesai" value="{{ $pesanan->tanggal_selesai }}">
+                                                </div>
+                                                <div class="mb-3">
+                                                    <label for="total_harga" class="form-label">Total Harga</label>
+                                                    <input class="form-control" type="number" inputmode="numeric" min="5000" step="5000" id="total_harga" name="total_harga" value="{{ (int) $pesanan->total_harga }}">
+                                                </div>
+                                                <button type="submit" class="btn btn-primary">Perbarui</button>
+                                            </form>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            {{-- <a href="{{ route('pesanan.edit', $pesanan->id) }}" class="btn btn-sm btn-warning"><i class="bi bi-pencil"></i></a> --}}
+
+                            <button type="button" class="btn btn-sm btn-danger" data-bs-toggle="modal" data-bs-target="#deleteUser{{ $pesanan->id }}">
+                                <i class="bi bi-trash"></i>
+                            </button>
+                            <div class="modal fade" id="deleteUser{{ $pesanan->id }}" tabindex="-1" aria-labelledby="deleteUserLabel" aria-hidden="true">
+                                <div class="modal-dialog">
+                                    <div class="modal-content">
+                                        <div class="modal-header">
+                                            <h5 class="modal-title" id="deleteUserLabel">Hapus Pengguna</h5>
+                                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                        </div>
+                                        <div class="modal-body">
+                                            Apakah Anda yakin ingin menghapus pesanan ini?
+                                        </div>
+                                        <div class="modal-footer">
+                                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Batal</button>
+                                            <form action="{{ route('pesanan.destroy', $pesanan->id) }}" method="post">
+                                                @csrf
+                                                @method('delete')
+                                                <button type="submit" class="btn btn-danger">Hapus</button>
+                                            </form>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
                         @endif
                         @endrole
                     </div>
